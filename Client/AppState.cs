d1f4918b32b66b2d17b446334aa052f18151cdb2
Yongsoo Na <newpullin@naver.com>
2020-10-30 09:10:38 +0900

    초기 커밋.

---
+++ Client/AppState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace LM2.Client
+{
+    public static class AppState
+    {
+        public static String token = "";
+        public static String name = "";
+
+    }
+}
---
+++ Shared/Libraries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LM2.Shared
+{
+    public class Libraries
+    {
+        public Libraries(int id, string name, string location, string tel)
+        {
+            this.lib_id = id;
+            this.lib_name = name;
+            this.location = location;
+            this.tel = tel;
+        }
+        public int lib_id { get; set; }
+        public string lib_name { get; set; }
+        public string location { get; set; }
+        public string tel { get; set; }
+    }
+
+    public class LSSSS
+    {
+        public Libraries[] libs { get; set; }
+    }
+}
